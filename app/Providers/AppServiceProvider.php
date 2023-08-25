f62350d75a4bc9ba5480db5965fb587e113a9e9a
sng0107 <133623422+sng0107@users.noreply.github.com>
2023-08-25 13:56:33 +0900

    change httpsスペル修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(\App::environment(['protection'])){
+        if (\App::environment(['production'])){
             \URL::forceScheme('https');
         }
 
