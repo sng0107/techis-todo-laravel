121af622e511b808447059223608ed1cdf1d3d53
sng0107 <133623422+sng0107@users.noreply.github.com>
2023-08-25 12:34:40 +0900

    change migraionsした

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         
-        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+        // \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 }
