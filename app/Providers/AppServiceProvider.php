b7b3fefd4a8ed2d9d4a5f290826377d9591a6a88
sng0107 <133623422+sng0107@users.noreply.github.com>
2023-08-25 12:28:33 +0900

    defaultStringLength(191)追記した

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 }
